8a648cdd5fca729a41fe5528dbecac2cb2e410ea
marten-mertan <mertan@mail.ru>
2021-07-21 06:23:21 +0300

    deploy on gh-pages

---
+++ pages/summon.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="page summon">
+        SUMMON
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Summon',
+        components: {
+        },
+        computed: {
+        },
+    };
+</script>
+
+<style lang="scss">
+    .summon {
+        display: flex;
+        flex-direction: column;
+        min-height: 100%;
+        width: 100%;
+        background: url('assets/images/bg_1.jpg') repeat top center;
+    }
+</style>
